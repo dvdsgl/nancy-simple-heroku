250f63803213d8850a0d92acc9699aeda9a13e13
BenHall <Ben@Benhall.me.uk>
2012-01-03 15:40:13 +0000

    Trying to get it to connect to the right host

--- src/Program.cs
+++ src/Program.cs
@@ -10,7 +10,7 @@
         static void Main()
         {
             var port = System.Environment.GetEnvironmentVariable("PORT");
-            var nancyHost = new NancyHost(new Uri("http://0.0.0.0:" + port));
+            var nancyHost = new NancyHost(new Uri("http://0.0.0.0:" + port), new Uri("http://0.0.0.0:" + port + "/"));
             nancyHost.Start();
 
             Console.WriteLine("Nancy now listening - navigating to http://0.0.0.0:" + port +".");
