f8d2ee8a04a97e84bb0643d1f371ce753c441f20
BenHall <Ben@Benhall.me.uk>
2012-01-10 13:59:59 +0000

    Listen on 0.0.0.0 as it helps when locally debugging

--- src/Program.cs
+++ src/Program.cs
@@ -10,7 +10,7 @@
         static void Main()
         {
             var port = System.Environment.GetEnvironmentVariable("PORT");
-            var nancyHost = new NancyHost(new Uri("http://deep-moon-1452.herokuapp.com:" + port), new Uri("http://deep-moon-1452.herokuapp.com:" + port + "/"));
+            var nancyHost = new NancyHost(new Uri("http://deep-moon-1452.herokuapp.com:" + port), new Uri("http://0.0.0.0:" + port));
             nancyHost.Start();
 
             Console.WriteLine("Nancy now listening - navigating to http://deep-moon-1452.herokuapp.com:" + port +".");
