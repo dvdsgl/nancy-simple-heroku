835abb3a49d63211743fdd1465b2172bd056bcfd
BenHall <Ben@Benhall.me.uk>
2012-01-11 02:16:21 +0000

    Moved hostnamed into a variable

--- src/Program.cs
+++ src/Program.cs
@@ -9,11 +9,13 @@
     {
         static void Main()
         {
+            var hostname = "deep-moon-1452";
+
             var port = System.Environment.GetEnvironmentVariable("PORT");
-            var nancyHost = new NancyHost(new Uri("http://deep-moon-1452.herokuapp.com:" + port), new Uri("http://0.0.0.0:" + port));
+            var nancyHost = new NancyHost(new Uri("http://" + hostname + ".herokuapp.com:" + port), new Uri("http://0.0.0.0:" + port));
             nancyHost.Start();
 
-            Console.WriteLine("Nancy now listening - navigating to http://deep-moon-1452.herokuapp.com:" + port +".");
+            Console.WriteLine("Nancy now listening - navigating to http://" + hostname +".herokuapp.com:" + port +".");
 
             var line = Console.ReadLine();
             while(line != "quit") {
